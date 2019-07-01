d5a19b7d2e0c710dada6476d8aeb265dbbc542e4
dispatchedx <spyros.boukis@outlook.com>
2019-07-01 16:06:55 +0300

    refactor into classes and completed the login window

--- login.py
+++ login.py
@@ -1,71 +1,103 @@
 from tkinter import *
+import mysqlFunctions
 
-#possible tables: recruiter, candidate, admin
-def check_login():
-    name,password,admin,recruiter,candidate='UNSET'
-    if name in admin and password:
-        login_admin()
-    if name in recruiter and password:
-        login_recruiter()
-    elif name in candidate and password:
-        login_candidate()
 
-def login_admin():
-    pass
+class AdminWindow:
+    #TODO make a new window or configure the current one?
+    def __init__(self, master):
+        self.master = master
+        master.title("Admin")
 
-def login_recruiter():
-    pass
 
-def login_candidate():
-    pass
+class RecruiterWindow:
+    def __init__(self, master):
+        self.master = master
+        master.title("Recruiter")
+
+
+
+class CandidateWindow:
+    def __init__(self, master):
+        self.master = master
+        master.title("Candidate")
+
+
 def test():
     print('works')
 
 
+class LoginWindow:
+    def __init__(self, master):
+        self.master = master
+        master.title("Database GUI")
+        self.type_of_user = None
+        self.create_widgets()
+        root.resizable(False, False)
+
+    def create_widgets(self):
+        self.left_space = Label(self.master)
+        self.left_space.grid(padx=10)
+        self.right_space = Label(self.master)
+        self.right_space.grid(column=3, padx=30)
+
+        self.welcome_label = Label(self.master, text='Please login')
+        self.welcome_label.grid(row=0, column=2, sticky=N)
+
+        self.username_label = Label(self.master, text='Username')
+        self.username_label.grid(row=2, column=1, sticky=E)
+        self.password_label = Label(self.master, text='Password')
+        self.password_label.grid(row=3, column=1, sticky=E)
+
+        self.input_username = StringVar()
+        self.username_entry = Entry(self.master, textvariable=self.input_username, width=20)
+        self.username_entry.grid(row=2, column=2, sticky=E)
+
+
+        self.input_password = StringVar()
+        # Password entry automatically hides what is typed
+        self.password_entry = Entry(self.master, textvariable=self.input_password, show='*', width=20)
+        self.password_entry.grid(row=3, column=2, sticky=E)
+        # Pressing Enter in the password entry calls login
+        self.password_entry.bind('<Return>', self.check_login)
+
+        self.show_password = BooleanVar()
+        self.show_password_checkbox = Checkbutton(self.master, text='Show password',
+                                                  variable=self.show_password,
+                                                  command=self.toggle_password)
+        self.show_password_checkbox.grid(row=4, column=2, sticky=E)
+
+        self.login_button = Button(self.master, text='Login', command=self.check_login)
+        self.login_button.grid(row=6, column=2, sticky=N + E + W + S)
+        self.login_button.bind()
+
+    def toggle_password(self):
+        if self.show_password.get():
+            self.password_entry.config(show='')
+        else:
+            self.password_entry.config(show='*')
+
+    def check_login(self, event=None):
+        """ Calls mysqlFunctions.login and opens the appropriate window depending on the type of user that
+            logged in. If the password if wrong it makes a new label and tells the user
+        """
+        self.type_of_user = mysqlFunctions.login(self.input_username.get(), self.input_password.get())
+        if self.type_of_user == 'recruiter':
+            root2 = Toplevel(self.master)
+            recruiter = RecruiterWindow(root2)
+
+        elif self.type_of_user == 'candidate':
+            pass
+            # candidate_window()
+        elif self.type_of_user == 'admin':
+            pass
+            # admin_window()
+        else:
+            self.wrong_password = Label(self.master, text='Wrong password or username', fg='red')
+            self.wrong_password.grid(row=1, column=1, columnspan=3, sticky=N)
+
+    print(mysqlFunctions.login('msmith', 'we3wd'))
+
+
 root = Tk()
-root.title('Database GUI')
-#root.winfo_toplevel().title('Database GUI')
-
-
-name_label = Label(root, text='Name')
-name_label.grid(row=1, sticky=W)
-password_label = Label(root, text='Password')
-password_label.grid(row=2, sticky=W)
-welcome_label = Label(root, text='Please login')
-welcome_label.grid(row=0, columnspan=2)
-
-input_name = StringVar()
-name_entry = Entry(root, textvariable=input_name, width=15)
-name_entry.grid(row=1, column=1, sticky=W)
-
-input_password = StringVar()
-password_entry = Entry(root, textvariable=input_password, show='*', width=15)
-password_entry.grid(row=2, column=1, sticky=W)
-
-
-def toggle_password():
-    if show_password.get():
-        password_entry.config(show='')
-    else:
-        password_entry.config(show='*')
-
-
-def off():
-    password_entry.config(show='*')
-
-
-show_password = BooleanVar()
-show_password_checkbox = Checkbutton(root, text='Show password', variable=show_password, command=toggle_password)
-show_password_checkbox.grid(row=3, column=1)
-
-
-def show():
-    """Works"""
-    p = input_password.get()
-    print(p)
-
-
-login_button = Button(root, text='Login', command=show)
-login_button.grid(row=4, columnspan=2)
-
+app = LoginWindow(root)
 root.mainloop()
